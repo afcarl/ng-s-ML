ee4df63b31bd10683d8b93c44d0d14f6044a9da4
Divyansh Jha <divyanshj.16@gmail.com>
2017-08-14 00:15:01 +0530

    add linearregcostfunc in ex5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -11,6 +11,7 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
+% size(grad)
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
@@ -19,7 +20,25 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+pred = X * theta;
+sqrErrors = (pred - y).^2;
+regularization_term = lambda * sum(theta(2:end) .^ 2)/(2*m);
+J = 1 / (2*m) * sum(sqrErrors) + regularization_term;
 
+% X
+grad = (X' * (pred - y))/m;
+temp = theta;
+grad(2:end) = grad(2:end) .+ (lambda/m) .* temp(2:end);
+% grad(2:n) = grad(2:n) + lambda / m * theta(2:n);
+
+
+% htheta = X * theta;
+% n = size(theta);
+% J = 1 / (2 * m) * sum((htheta - y) .^ 2) + lambda / (2 * m) * sum(theta(2:n) .^ 2);
+
+% grad = 1 / m * X' * (htheta - y);
+% size(grad)
+% grad(2:n) = grad(2:n) + lambda / m * theta(2:n);
 
 
 
