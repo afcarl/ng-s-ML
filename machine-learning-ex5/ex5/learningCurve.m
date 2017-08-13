149a53b3386c96a250444b1bf3048773c70ce1ba
Divyansh Jha <divyanshj.16@gmail.com>
2017-08-14 00:15:31 +0530

    add learning curve in ex5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -42,13 +42,20 @@ error_val   = zeros(m, 1);
 %
 % Hint: You can loop over the examples with the following:
 %
-%       for i = 1:m
+      for i = 1:m
 %           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
+          temp_train = X(1:i, :);
+          temp_labels = y(1:i);
+          theta = trainLinearReg(temp_train,temp_labels,lambda);
+          [J_train , grad] = linearRegCostFunction(temp_train,temp_labels,theta,0);
+          error_train(i) = J_train;
+          [J_cv, grad] = linearRegCostFunction(Xval,yval,theta,0)
+          error_val(i) = J_cv
+
 %           % error_train(i) and error_val(i)
 %           ....
 %           
-%       end
+      end
 %
 
 % ---------------------- Sample Solution ----------------------
